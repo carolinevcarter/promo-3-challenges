c3d582d4843553ad6298ad0efa91a5cd794ac0dc
Caroline Carter <carolinevcarter@gmail.com>
2014-07-10 00:00:24 +0200

    YES HAHA

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,14 @@
 def bank_score
-  # TODO: Use Random to get a new random score
+  r = Random.new
+  return r.rand(16...22)
 end
 
 def pick_card
-  # TODO: Use Random to get a new random card
+  r = Random.new
+  return r.rand(1...12)
 end
 
 def game_outcome(bank, score)
   # TODO: Take the bank and the score and output an array containing the bank and then the score
+  return [bank, score]
 end
