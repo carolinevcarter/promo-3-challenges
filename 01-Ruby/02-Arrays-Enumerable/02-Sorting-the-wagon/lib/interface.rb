a4e4a6d96a4c8447fa432098328e3295dd920eaa
Caroline Carter <carolinevcarter@gmail.com>
2014-07-09 22:38:31 +0200

    Completed Interface Exercise

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -1,69 +1,56 @@
 require_relative "wagon_sort"
 
-students = []
-
-begin
-  puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
-  name = gets
-  name.chomp! if name
-
-  # TODO: Add the name we just gotto the students array
-
-end while name != ""
-
 ################################################
 
 students_list = []
 
 puts "Enter first name? or press <Enter>"
 name = gets.chomp
-end
+
 
 until name == ""
   students_list << name
-  puts "Sumbit a name or quit with <enter>"
+  puts "Sumbit a name or quit with <Enter>"
   name = gets.chomp
 end
-
 
 #include a congrats message
 if students_list.size == 1
   congrats_message = "Congratulations! your wagon has 1 student"
-  else
+else
   congrats_message = "Congratulations! your wagon has #{students_list.size} students"
-  end
+end
 
-  puts congrats_message
+puts congrats_message
 
-  #display the sorted student list
-  sorted_students = wagon_sort(students_list)
 
-  # Write a message with the student names
-  message = ""
-  for students in sorted_students
+# Trier le tableau
+sorted_students = wagon_sort(students_list)
+
+
+# Construire le message 'alice, bob, joey, and pepita'
+message = ""
+
+for student in sorted_students
 
   #Add the student to the message, separated by a coma
   #if it's the last, don't forget
   if student == sorted_students.last
+    message += student
+  #if it's the second to last
+  elsif student == sorted_students[sorted_students.size - 2]
     message += student + " and "
-
   #otherwise
   else
-    message += student + " , "
+    message += student + ", "
   end
 end
 
 puts message
 
-#with join and with class!
+# avec join et avec classe !
+first_part = sorted_students[0..(sorted_students.size-2)].join(", ")
 
-first_part = sorted_students[0..(sorted_students.size)].join(" , ")
+final_message = "#{first_part} and #{sorted_students.last}"
 
-final_message = "#{first_part}" and "#{sorted_students.last}"
 puts final_message
-
-
-
-# TODO: Call `wagon_sort` method and display the sorted student list
-
-#Type a student name, type another student name, type another student name
