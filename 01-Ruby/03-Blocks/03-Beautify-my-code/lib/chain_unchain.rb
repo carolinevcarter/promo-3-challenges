df0ad700f0ee96bda9debbf057014ffae7a1d273
Caroline Carter <carolinevcarter@gmail.com>
2014-07-10 16:04:18 +0200

    Completed exercise

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -4,7 +4,14 @@ end
 #Refractor
 
 def quote_prime_numbers(n)
-
+  #TODO: refactor this method
+  prime_numbers = (1..n).find_all do |i|
+    selector =  (2..i - 1).select {|k| i % k == 0 }
+    selector.count == 0
+    # k correspondent à tous les nombres entre 2 et i-1 pour vérifier qu'il n'est pas divisible.
+  end
+  prime_numbers.map { |prime_num| "#{prime_num} is prime" }
 end
+
 
   #TODO: refactor this method
