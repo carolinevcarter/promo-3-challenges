b5b5a35d59b2fe85fa251f59aa583dcde508f9ca
Caroline Carter <carolinevcarter@gmail.com>
2014-07-11 14:29:46 +0200

    McDo exercise

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,34 @@
+
+McDonalds = Hash["Cheeseburger" => 290,
+  "Big Mac" => 300,
+  "Mc Bacon" => 400,
+  "Royal Cheese" => 130,
+  "French fries" => 130,
+  "Potatoes" =>130,
+  "Coca" => 160,
+  "Sprite" => 170]
+
 def poor_calories_counter(burger, side, beverage)
-  #TODO: return number of calories for this mcDonald order
+  sum = 0
+  McDonalds.each do |x,y|
+    if x == burger || x == side || x == beverage
+      sum += y
+    end
+  return sum
 end
+#TODO: return number of calories for this mcDonald order
 
 def calories_counter(*orders)
-  #TODO: return number of calories for a less constrained order
+  sum = 0
+  orders.each do |x|
+    if x == "Happy Meal"
+      sum += poor_calories_counter("Cheese Burger", "French fries", "Coca")
+    elsif x == "Best Of Big Mac"
+      sum += poor_calories_counter("Big Mac", "French fries", "Coca")
+    elsif x == "Best Of Royal Cheese"
+      sum += poor_calories_counter("Royal Cheese", "Potatoes", "Sprite")
+    else
+      sum += McDonalds[x]
+    end
+  return sum
 end
