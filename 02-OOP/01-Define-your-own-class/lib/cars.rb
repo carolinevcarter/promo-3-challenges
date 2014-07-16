53ddff8f2a8ad64eeb9418bb963731e2f765b257
Caroline Carter <carolinevcarter@gmail.com>
2014-07-16 11:42:13 +0200

    Car exercise

---
+++ 02-OOP/01-Define-your-own-class/lib/cars.rb
@@ -0,0 +1,13 @@
+class Cars
+  def initialize(color, brand, power)
+    @color = color
+    @brand = brand
+    @power = power
+    @engine_started = false
+  end
+
+  def engine
+    @engine_started = true
+  end
+end
+
