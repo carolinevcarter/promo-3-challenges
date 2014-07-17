35fe8b8c6c941f2c1ca7046241a129da7729d7ca
Caroline Carter <carolinevcarter@gmail.com>
2014-07-17 15:12:25 +0200

    Restaurant exercise completion

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,35 @@
 class Restaurant
-  
-  #TODO add relevant accessors if necessary
-  
+
+attr_reader :city
+attr_reader :name
+attr_reader :type_of_food
+attr_reader :average_rating
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @type_of_food = type_of_food
+    @all_ratings = []
+    @average_rating = 0
   end
-  
-  #TODO: implement #filter_by_city and #rate methods
-  
-end
+
+  def rate(restaurant_rating)
+    sum = 0
+    @all_ratings << restaurant_rating.to_f
+    @all_ratings.each do |rate|
+      sum += rate
+    end
+    @average_rating = sum / @all_ratings.length
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.each do |restaurant|
+      if restaurant.city == city
+        true
+      else
+        false
+      end
+    end
+   end
+  end
