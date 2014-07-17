1954056b7ee6542fbb88282cf1f3acf204d9742c
Caroline Carter <carolinevcarter@gmail.com>
2014-07-17 15:44:05 +0200

    Almost finisheddddd

--- 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
@@ -1,26 +1,30 @@
 module DemoModule
 
-  class DemoClass  
-    
+  class DemoClass
+
     def self.introduce
-      #TODO: choose return between 1, 2, 3
+      3
     end
+    #TODO: choose return between 1, 2, 3
 
     def introduce
-      #TODO: choose return between 1, 2, 3
+      1
     end
+     #TODO: choose return between 1, 2, 3
 
   end
-  
-  def self.introduce
-    #TODO: choose return between 1, 2, 3
-  end
+
+    def self.introduce
+      2
+    end
+  #TODO: choose return between 1, 2, 3
 
 end
 
 def has_tierce?
   first = DemoModule::introduce
-  second = DemoModule::DemoClass.introduce      
+  second = DemoModule::DemoClass.introduce
   third = DemoModule::DemoClass.new.introduce
   [first, second, third] == [1, 2, 3]
 end
+
