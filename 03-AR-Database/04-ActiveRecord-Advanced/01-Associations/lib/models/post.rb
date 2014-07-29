fbe9849e169e9e5c4ccad6668a11d06c02d4c5cb
Caroline Carter <carolinevcarter@gmail.com>
2014-07-29 15:25:23 +0200

    Completed models section

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   # TODO: add association to the user model
+  belongs_to :user
 end
+
+
--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  # TODO: add associations to the Post model
+  def up
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+    end
+  end
 end
+
+
+
+ # TODO: add associations to the Post model
