20175084c38fc796112a556e35f56a3fdcdec491
Caroline Carter <carolinevcarter@gmail.com>
2014-08-04 15:42:10 +0200

    To the left to the left

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,3 +1,14 @@
+//$("#message-wrapper").load(function() {
+  //  $("#message-wrapper").fadeOut(5000);
+//});
+
+$( "#message-wrapper" ).hide().slideDown(5000);
+
+$( "#message-wrapper" ).click(function() {
+  $("#message-wrapper").slideUp(5000);
+});
+
+
 /*
   *
   * TODO: add your code here!
