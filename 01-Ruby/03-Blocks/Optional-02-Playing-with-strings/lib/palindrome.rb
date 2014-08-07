83a08338b65c3379f6f516e5d8fc38648fb9b5d0
Caroline Carter <carolinevcarter@gmail.com>
2014-08-07 15:07:37 +0200

    Paindroming roaming

--- 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
@@ -1,8 +1,17 @@
-def palindrome?(a_string)
   #TODO: check if a_string is a palindrome
+def palindrome?(a_string)
+  if string.length == 1 || string.length ==
+    true
+  else
+    if string [0] == string[-1]
+      palindrome?(string[1..-2])
+    else
+      false
+    end
+  end
 end
 
-  
+
 # Testing palindrome? function
 puts "#{palindrome?("A man, a plan, a canal -- Panama")}" #=> true
 puts "#{palindrome?("Madam, I'm Adam!")}" # => true
